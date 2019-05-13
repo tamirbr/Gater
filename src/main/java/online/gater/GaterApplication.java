f841813f9c41db94f22c3a7a453c0c44296413b0
tamirbr <tamirbr2@hotmail.com>
2019-05-13 09:17:56 +0300

    new files

--- src/main/java/online/gater/GaterApplication.java
+++ src/main/java/online/gater/GaterApplication.java
@@ -1,13 +1,41 @@
 package online.gater;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import lombok.extern.java.Log;
+
+@Log
 @SpringBootApplication
-public class GaterApplication {
+public class GaterApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
+        startH2Server();
 		SpringApplication.run(GaterApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        startH2Server();
+        return application.sources(GaterApplication.class);
+    }
+
+    private static void startH2Server() {
+        try {
+            Server h2Server = Server.createTcpServer().start();
+            if (h2Server.isRunning(true)) {
+                log.info("H2 server was started and is running.");
+            } else {
+                throw new RuntimeException("Could not start H2 server.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to start H2 server: ", e);
+        }
+    }
 
 }
